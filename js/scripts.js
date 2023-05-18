d837f4b50a1ef6ef4cc134da0484f8d42f473d90
maicol <maicolpr19@gmail.com>
2023-05-18 17:34:55 -0500

    avance delicioso

--- js/scripts.js
+++ js/scripts.js
@@ -137,3 +137,21 @@ window.onload = function(){
     renderCarrito()
   }
 }
+
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").forEach(producto =>{
+
+        producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?producto.classList.remove("filtro")
+            :producto.classList.add("filtro")
+      })
+
+  }
+
+
+})
